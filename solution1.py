9f6ff4f19d60a2f334e242388cc7babb552b3ec5
Roger Earl <rogerearl@me.com>
2023-04-19 19:46:53 -0700

    Finished algorithms 1 and 2. Started 3.

--- solution1.py
+++ solution1.py
@@ -1,1 +1,9 @@
 # Write code for algorithm 1 below
+def recur1(n):
+    if n == 0:
+        return 0
+    else:
+        print(n)
+        recur1(n - 1)
+    
+recur1(10)
--- solution3.py
+++ solution3.py
@@ -1,1 +1,9 @@
 # Write code for algorithm 3 below
+def fibby_seq(n):
+    if n == 0:
+        return n
+    elif n == 2:
+        return 1
+    else:
+        return fibby_seq(n-1)+fibby_seq(n-2)
+print(fibby_seq(2))
