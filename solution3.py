76bda4d2fbc9db8eb27d28eb71788a2f03ff16f6
Roger Earl <rogerearl@me.com>
2023-04-19 19:55:20 -0700

    Finished algorithm 3

--- solution3.py
+++ solution3.py
@@ -1,9 +1,13 @@
 # Write code for algorithm 3 below
 def fibby_seq(n):
-    if n == 0:
+    if n <= 1:
         return n
     elif n == 2:
         return 1
     else:
-        return fibby_seq(n-1)+fibby_seq(n-2)
+        return fibby_seq(n-1) + fibby_seq(n-2)
 print(fibby_seq(2))
+print(fibby_seq(3))
+print(fibby_seq(5))
+print(fibby_seq(8))
+
