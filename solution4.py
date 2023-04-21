f3cb95d6a1992405b2842544c911e0864247a1f4
Roger Earl <rogerearl@me.com>
2023-04-20 23:01:45 -0700

    Finished 4 and 5

--- solution4.py
+++ solution4.py
@@ -1,1 +1,4 @@
 # Write code for algorithm 4 below
+def power(a, b):
+    return a^b
+print(power(2, 3))
--- solution5.py
+++ solution5.py
@@ -1,1 +1,10 @@
 # Write code for algorithm 5 below
+def palindrome(str):
+    str = str.join(char.lower() for char in str if char.isalnum())
+    return str == str[::-1]
+
+
+word1 = "racecar"
+word2 = "Python"
+print(palindrome(word1))
+print(palindrome(word2)) 
